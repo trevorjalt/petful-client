b8a362d77aedcba9d55c7d975b2db03539f7e7aa
Trevor Alt <trevorjalt@gmail.com>
2020-11-06 21:30:29 -0500

    bugs2

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 
 export default {
-    REACT_APP_API_BASE: process.env.REACT_APP_API_BASE || 'http://localhost:8080/api'
+    API_ENDPOINT='https://petful-api-tja.herokuapp.com'
 }
