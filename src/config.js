799a5f04746b51ce744f1162dbeb4e97163b9252
Trevor Alt <trevorjalt@gmail.com>
2020-11-08 18:53:58 -0500

    shipping

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 
 export default {
-    // API_ENDPOINT: 'https://petful-api-tja.herokuapp.com/api'
-    API_ENDPOINT: 'http://localhost:8000/api'
+    API_ENDPOINT: 'https://petful-api-tja.herokuapp.com/api'
 }
