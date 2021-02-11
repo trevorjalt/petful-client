f2c9a1b1f326e3e957721fb39d698d41a4a91fb1
Trevor J Alt <trevor@trevorjalt.com>
2021-02-11 04:51:36 -0500

    config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 require('dotenv').config()
 
 export default {
-    API_ENDPOINT: (process.env.REACT_APP_ENV === 'production')
-        ? 'https://petful-api-tja.herokuapp.com/api'
-        : 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://petful-api-tja.herokuapp.com/api'
 }
